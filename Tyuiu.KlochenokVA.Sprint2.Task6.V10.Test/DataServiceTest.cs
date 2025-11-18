5e8ca1495c85e831ae5c37f7390ed1aa1c29d70b
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-18 22:28:50 +0500

    Task6

---
+++ Tyuiu.KlochenokVA.Sprint2.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib;
+namespace Tyuiu.KlochenokVA.Sprint2.Task6.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("13.10.2023", ds.FindDateOfPreviousDay(2023, 10, 14));
+        }
+    }
+}
---
+++ Tyuiu.KlochenokVA.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint2.Task6.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Клоченок В. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Получение результата из switch                                                  *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #10                                                            *");
+            Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дата некоторого дня характеризуется тремя натуральными числами:        *");
+            Console.WriteLine("* g (год), m (порядковый номер месяца) и n (число). По заданным g, n и m *");
+            Console.WriteLine("* определить дату предыдущего дня. Заданный год не является високосным.  *");
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите номер дня: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер месяца: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер года: ");
+            int g = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Дата предыдущего дня: " + ds.FindDateOfPreviousDay(g, m, n));
+
+            Console.ReadKey();
+        }
+    }
+}
