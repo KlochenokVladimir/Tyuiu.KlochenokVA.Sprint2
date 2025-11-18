52346c191a140c4a212bf66810c7a3e390c717b9
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-18 22:39:32 +0500

    Task7

---
+++ Tyuiu.KlochenokVA.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KlochenokVA.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.KlochenokVA.Sprint2.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0;
+            double y = 0.5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
