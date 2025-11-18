593982cd54baa599afca2b4009fa2b8a01f73be3
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-18 21:55:35 +0500

    fix

--- Tyuiu.KlochenokVA.Sprint2.Task3.V11.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint2.Task3.V11.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
 namespace Tyuiu.KlochenokVA.Sprint2.Task3.V11.Lib
 {
     public class DataService : ISprint2Task3V11
@@ -6,14 +7,23 @@ namespace Tyuiu.KlochenokVA.Sprint2.Task3.V11.Lib
         public double Calculate(double x)
         {
             double y = 0;
+
             if (x > 0)
-                y = x + Math.Pow((x - 15) / (2 - 19), x);
-            if (x == 0)
+            {
+                y = x + Math.Pow((x - 15.0) / (x - 19.0), x);
+            }
+            else if (x == 0)
+            {
                 y = (x * x - Math.Cos(x * x) + 10) / (x * x - Math.Sin(x * x) + 12);
-            if (x > -15 && x < 0)
-                y = Math.Pow(1 + 1 / (x * x), x);
-            if (x < -15)
-                y = x + 10 * x - 1 / x;
+            }
+            else if (x > -15 && x < 0)
+            {
+                y = Math.Pow(1 + 1.0 / (x * x), x);
+            }
+            else if (x < -15)
+            {
+                y = x + 10 * x - 1.0 / x;
+            }
 
             return y;
         }
