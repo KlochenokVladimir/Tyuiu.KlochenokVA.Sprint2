4c6ecc3943d9b18fe252cbeab690de6960144d7b
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-18 22:32:18 +0500

    fix

--- Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib/DataService.cs
@@ -5,45 +5,32 @@ namespace Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
-            string res;
-            switch (n)
+            if (n == 1)
             {
-                case 1:
-                    switch (m)
+                if (m == 1)
+                {
+                    m = 12;
+                    n = 31;
+                    g--;
+                }
+                else
+                {
+                    m--;
+                    n = m switch
                     {
-                        case 1:
-                            n = 31;
-                            m = 12;
-                            g--;
-                            break;
-                        default:
-                            m--;
-                            switch (m)
-                            {
-                                case 1:
-                                case 3:
-                                case 5:
-                                case 7:
-                                case 8:
-                                case 10:
-                                    n = 31;
-                                    break;
-                                case 2:
-                                    n = 28;
-                                    break;
-                                default:
-                                    n = 30;
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
-                default:
-                    n--;
-                    break;
+                        1 or 3 or 5 or 7 or 8 or 10 or 12 => 31,
+                        2 => 28,
+                        _ => 30
+                    };
+                }
             }
-            res = ($"{n}.{m}.{g}");
-            return res;
+            else
+            {
+                n--;
+            }
+
+            
+            return $"{n:00}.{m:00}.{g}";
         }
     }
 }
