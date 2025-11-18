36796002b6cebd20cce7e5d9220cb0b1411a07d5
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-18 22:30:03 +0500

    fix

--- Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.KlochenokVA.Sprint2.Task6.V10.Lib
 {
-    public class DataService : ISprint2Task5V10
+    public class DataService : ISprint2Task6V10
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
